322e0855fb163ce06800489fcce6208c30369ba0
Brandon Weaver <keystonelemur@gmail.com>
2013-06-17 22:29:45 -0500

    Fleshed out skeleton

---
+++ mime.rb
@@ -0,0 +1,38 @@
+# Mime Base program
+#
+# I'll have to work on getting the library done. First some
+# experimentation in IRB
+
+# Begin recording a new Macro
+def record
+end
+
+# Stop recording a Macro
+def stop
+end
+
+# Run a Macro
+def run
+end
+
+# Generate Documentation based on the specified Mime file
+# 
+# Will eventually need to have exporting options for rendering
+# as different formats such as MD or TXT
+def document
+end
+
+# Merge the contents of two Macros and make a new one based on
+# them
+def merge
+end
+
+# Edit the contents of the Mime, debating if I just want to export
+# it to the default editor or make a custom editor for this one.
+def edit
+end
+
+# Delete a Macro. Confirmation? We're in Unix. Ain't nobody got time
+# for that!
+def delete
+end
