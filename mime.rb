9c09999487c5f2d1120ea75a784f2fc57e809bd1
Brandon Weaver <keystonelemur@gmail.com>
2013-06-18 23:07:30 -0500

    Added Markdown Documentation - github-markdown now required

--- mime.rb
+++ mime.rb
@@ -1,7 +1,10 @@
+#!/usr/local/bin/ruby
 # Mime Base program
 #
 # I'll have to work on getting the library done. First some
 # experimentation in IRB
+
+require 'github/markdown'
 
 # Begin recording a new Macro
 def record(macro_name)
@@ -19,10 +22,10 @@ def record(macro_name)
       break
     end
 
-    commands << cmd + "\n"
+    commands << cmd
 
     # Execute command, print results
-    puts `#{cmd}`
+    puts `#{cmd}` unless cmd =~ /^#/
   end
 
   File.open("#{macro_name}.mime", 'w'){ |f| f.write(commands) }
@@ -32,14 +35,34 @@ end
 def run(macro_name)
   macro = File.open("#{macro_name}.mime",'r')
 
-  macro.each_line{ |cmd| puts `#{cmd}` unless cmd =~ /^$/ }
+  macro.each_line do |cmd| 
+    unless cmd =~ /^$/ || cmd =~ /^#/  
+      puts `#{cmd}` unless cmd =~ /^$/
+    end
+  end
 end
 
 # Generate Documentation based on the specified Mime file
 # 
 # Will eventually need to have exporting options for rendering
 # as different formats such as MD or TXT
-def document
+def document(macro_name)
+  macro = File.open("#{macro_name}.mime",'r')
+  docs  = File.open("doc-#{macro_name}.html",'w')
+
+  str = macro.each_line.inject("") do |str, line|
+    if line =~ /^#/
+      str << line.gsub(/^# /,'')
+    else
+      str << "    #{line}"
+    end
+  end
+  
+  # I'll probably want to put a basic stylesheet in here later, you know,
+  # to make it look a tad classier.
+
+  # Insert the string into the docs
+  docs.puts GitHub::Markdown.render(str)
 end
 
 # Merge the contents of two Macros and make a new one based on
